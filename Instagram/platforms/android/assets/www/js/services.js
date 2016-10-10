4419b8aed90cd16923bcf4759b13ef0b59b713c4
Chu Nguyen Khuong <t4chng00@students.oamk.fi>
2016-10-10 18:55:08 +0300

    Init

---
+++ Instagram/platforms/android/assets/www/js/services.js
@@ -0,0 +1,221 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var chats = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'img/ben.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'img/max.png'
+    }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'img/adam.jpg'
+    }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+    }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+    }];
+
+    return {
+        all: function () {
+            return chats;
+        },
+        remove: function (chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function (chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
+            }
+            return null;
+        }
+    };
+})
+.factory('Profile', function () {
+    var Profile = {
+        name: 'Brandon Nguyen',
+        face: 'img/profile.jpg',
+    }
+
+    var length = 0;
+
+    return {
+        getProfile: function () {
+            return Profile;
+        },
+        setProfile: function (profile) {
+            Profile = profile;
+        },
+        resetProfile: function () {
+            Profile = {};
+        },
+        addLength: function () {
+            length += 1;
+        },
+        getLength: function () {
+            return length;
+        }
+    }
+})
+.factory('Posts', function () {
+
+    
+
+    var posts = [{
+       
+        name: 'Ben Sparrow',
+        comment: 'Check this out',
+        face: 'img/ben.png',
+        pic: 'img/wall1.jpg'
+    }, {
+       
+        name: 'Max Lynx',
+        comment: 'Some cool shit',
+        face: 'img/max.png',
+        pic: 'img/wall2.jpg'
+    }, {
+        
+        name: 'Adam Bradleyson',
+        comment: 'Nice road',
+        face: 'img/adam.jpg',
+        pic: 'img/wall3.jpg'
+    }, {
+        name: 'Perry Governor',
+        comment: 'WOW!',
+        face: 'img/perry.png',
+        pic: 'img/wall4.jpg'
+    }];
+    return {
+        all: function () {
+            return posts;
+        },
+        addPost: function (profile, comment,image) {
+            posts.unshift({
+             
+                name: profile.name,
+                comment: comment,
+                face: profile.face,
+                pic: image
+            });
+        },
+    };
+})
+.factory('Pictures', function () {
+    var pictures = ['img/pic1.jpg', 'img/pic2.jpg', 'img/pic3.jpg', 'img/pic4.jpg', 'img/pic5.jpg', 'img/pic6.png', 'img/pic7.jpg', 'img/pic8.jpg', 'img/pic9.jpg'];
+    return {
+        all: function () {
+            return pictures;
+        },
+        addImg: function (image) {
+            pictures.unshift(image);
+        }
+    };
+})
+.factory('Camera', function ($q) {
+
+    return {
+        getPicture: function (options) {
+            var q = $q.defer();
+
+            navigator.camera.getPicture(function (result) {
+                q.resolve(result);
+            }, function (err) {
+                q.reject(err);
+            }, options);
+
+            return q.promise;
+        }
+    }
+
+})
+.service('LoginService', function ($q, $http, Profile) {
+    return {
+        loginUser: function(name, pw) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+
+            var link = 'http://khuongstagram.herokuapp.com/users/login';
+
+            $http.post(link, { username: name, password: pw }).then(function (res) {                
+                if (res.data == false)
+                {
+                    deferred.reject('Wrong credentials.');
+                }
+                else
+                {
+                    Profile.setProfile(res.data);
+                    console.warn(res.data);
+                    deferred.resolve('Welcome ' + name + '!');
+                }
+            });
+    
+            promise.success = function (fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function (fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+
+            return promise;
+        }
+    }
+})
+.service('SignupService', function ($q, $http,Profile) {
+    return {
+        createUser: function (user) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+
+            var link = 'http://khuongstagram.herokuapp.com/users/create';
+
+            $http.post(link, { username: user.username, password: user.password, firstname: user.firstname, lastname: user.lastname }).then(function (res) {
+                
+                if (res.data == false) {
+                    deferred.reject('Wrong credentials.');
+                }
+                else {
+                    var link = 'http://khuongstagram.herokuapp.com/users/' + user.username;                    
+                    $http.get(link).then(function (res) {                        
+                        if (res.data.length == 0) {
+                            deferred.reject('Something went wrong.');
+                        }
+                        else {
+                            Profile.setProfile(res.data[0]);
+                            deferred.resolve('Welcome ' + name + '!');
+                        }
+                    });
+                }
+            });
+
+            promise.success = function (fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function (fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+
+            return promise;
+        }
+    }
+});
---
+++ Instagram/www/js/controllers.js
@@ -0,0 +1,134 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function ($scope, Posts) {
+    $scope.posts = Posts.all();
+    $scope.like = true;
+    
+
+})
+.controller('LikeCtrl', function ($scope, Posts) {
+    $scope.posts = Posts.all();
+})
+
+
+.controller('SearchesCtrl', function ($scope, Pictures) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.pictures = Pictures.all();
+
+})
+.controller('ProfileCtrl', function ($rootScope,$scope, Pictures, Profile, Posts, Camera, $state) {
+    $scope.profile = Profile.getProfile();
+    $scope.pictures = Pictures.all();
+    $scope.posts = Posts.all();
+    $scope.like = true;
+    $rootScope.$on('reload_profile', function () {
+        $scope.profile = Profile.getProfile();        
+    });
+    $scope.toggleLike = function () {
+        $scope.like = $scope.like === false ? true : false;
+    };
+    $scope.signout = function () {
+        Profile.resetProfile();
+        $state.go('login');
+    }
+})
+.controller('CameraCtrl', function ($scope, Camera, Pictures, Posts, Profile, $state) {
+    $scope.image = null;
+
+    $scope.data = {};
+
+    $scope.submit = function () {
+        Posts.addPost(Profile.getProfile(), $scope.data.comment, $scope.image);
+        $scope.image = null;
+        $scope.data = {};
+        $state.go('tab.home');
+    };
+
+    $scope.takePicture = function (options) {
+        
+        var options = {
+            quality: 75,
+            targetWidth: 200,
+            targetHeight: 200,
+            destinationType:1,
+            sourceType: 1
+        };
+
+        Camera.getPicture(options).then(function (imageData) {
+            console.warn(imageData);
+            alert(imageData);
+            $scope.image = imageData;
+            Pictures.addImg($scope.image);
+        }, function (err) {
+            console.log(err);
+        });
+    };
+    $scope.takePictureGallery = function (options) {
+
+        var options = {
+            quality: 75,
+            targetWidth: 200,
+            targetHeight: 200,
+            destinationType: 1,
+            sourceType: 0
+        };
+
+        Camera.getPicture(options).then(function (imageData) {
+            alert(imageData);
+            console.warn(imageData);
+            $scope.image = imageData;
+            Pictures.addImg($scope.image);
+        }, function (err) {
+           console.log(err);
+        });
+    };
+
+})
+.controller('LoginCtrl', function ($rootScope, $scope, LoginService, $ionicPopup, $state) {
+    $scope.data = {};
+
+    $scope.login = function () {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function (data) {            
+            $rootScope.$broadcast('reload_profile');
+            $state.go('tab.home');
+            $scope.data = {};
+        }).error(function (data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+    }
+
+    $scope.signup = function () {
+        $state.go('signup');
+    }
+})
+.controller('SignupCtrl', function ($rootScope,$scope, SignupService, $ionicPopup, $state) {
+    $scope.data = {};
+
+    $scope.back = function () {
+        $state.go('login');
+    }
+
+    $scope.create = function () {
+        SignupService.createUser($scope.data).success(function (data) {
+            $rootScope.$broadcast('reload_profile');
+            $state.go('tab.home');
+            $scope.data = {};
+        }).error(function (data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Create failed!',
+                template: 'Something went wrong!'
+            });
+        });
+    }
+})
+
